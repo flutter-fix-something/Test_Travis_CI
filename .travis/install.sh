304145cf1967027429e11013e80bff867033dcd3
Caijinglong <cjl_spy@163.com>
2020-01-19 16:11:01 +0800

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 cd $ROOT
 wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.12.13+hotfix.5-stable.zip
 
-unzip -d flutter flutter_macos_v1.12.13+hotfix.5-stable.zip
+unzip flutter_macos_v1.12.13+hotfix.5-stable.zip
 
 FLUTTER=$PWD/flutter/bin/flutter
 
